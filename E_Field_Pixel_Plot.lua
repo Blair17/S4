4a81c2898de67a852833a2b244447a1bab3e6359
Blair17 <48489837+Blair17@users.noreply.github.com>
2022-10-29 16:01:11 +0100

    Update E_Field_Pixel_Plot.lua

--- E_Field_Pixel_Plot.lua
+++ E_Field_Pixel_Plot.lua
@@ -31,21 +31,16 @@ local filez = io.open("parameter_z.csv", "w")
 printlam = 1
 
   for z = -200, 200, 1 do 
-    
       S = S4.NewSimulation()
       S:SetLattice(p)
-
       S:SetNumG(nharm)
       S:UsePolarizationDecomposition()
 
       S:AddMaterial("GratingMaterial", {gratingindex^2,0}) -- ITO
       S:AddMaterial("Air", {1,0})
       S:AddMaterial("Glass", {glassindex^2,0})
-
       S:AddLayer('AirAbove', 0, 'Air') -- name, thickness, background material
-
       S:AddLayer('Grating', gratingthickness, 'Air') 
-    
       S:SetLayerPatternRectangle('Grating', 'GratingMaterial', {0,0}, 0, {ridgewidth*0.5,0})
 
       --[[
@@ -62,23 +57,18 @@ printlam = 1
         S:SetLayerPatternRectangle('Grating', 'GratingMaterial', {-250,0}, 0, {p*0.03*0.5,0})
        ]]
       --S:AddLayer('Grating1', gratingthickness, 'GratingMaterial')
-
          --S:AddLayer('GlassSubstrate', 0, 'Glass')
 
       S:AddLayer('AirBelow', 0, 'Air')
-
       S:SetExcitationPlanewave({0,0},      -- incidence angles (spherical coords: phi [0,180], theta [0,360])
                          {TEamp,0},  -- TE-polarisation amplitude and phase (in degrees)
                          {TMamp,0})  -- TM-polarisation amplitude and phase
     
-
      -- COMPUTE AND SAVE TRANSMISSION / REFLECTION
-
       for x = 1, 1000, 1 do
 	      freq = 1/lambda
 
 	      S:SetFrequency(freq)
-  
 	      inc, back = S:GetPowerFlux('AirAbove', 20)
 	      forward, backward = S:GetPowerFlux('AirBelow', 20)
 	      refl = - back/inc
@@ -93,11 +83,10 @@ printlam = 1
 
 	      filespectrum:write(Eyr, ',') 
          filex:write("\n", x) 
-             
-     
-         end
+      end
 
       printlam = 0
+      -- Test code 
 
       filespectrum:write("\n") 
       filez:write("\n", z)
